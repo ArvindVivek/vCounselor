8bbcf5e06701728fbfe38385b36406182ad72291
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-07-25 12:45:01 -0700

    added college map view and scrollview

--- vCounselor/app/screens/resources_screen/resources_screen.tsx
+++ vCounselor/app/screens/resources_screen/resources_screen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions, ScrollView } from "react-native";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import {
   createAppContainer,
@@ -25,10 +25,14 @@ export class ResourcesScreen extends React.Component {
   render() {
     return (
       <Layout style={styles.container}>
-        <Card style={styles.cardStyle}>
-          <Text>College Map Search</Text>
-        </Card>
-        <WebView style={styles.webStyle} source={{ uri: COLLEGEMAPS_URL }} />
+        <ScrollView>
+          <Card style={styles.titleCard}>
+            <Text>College Map Search</Text>
+          </Card>
+          <View style={styles.webViewStyle}>
+            <WebView style={styles.webViewStyle} source={{ uri: COLLEGEMAPS_URL }} />
+          </View>
+        </ScrollView>
       </Layout>
     );
   }
@@ -40,14 +44,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  cardStyle: {
+  titleCard: {
     width: deviceWidth-20,
     alignItems: 'center',
     borderRadius: 15,
+    marginTop: 10,
+    marginBottom: 10,
   },
-  webStyle: {
-    marginTop: 5,
-    width: deviceWidth-23,
-    height: 200,
+  webViewStyle: {
+    width: deviceWidth-20,
+    alignItems: 'center',
+    borderRadius: 150,
+    height: deviceHeight/1.5,
   }
 });
