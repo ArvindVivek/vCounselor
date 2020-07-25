d60617400b8cb2e9a3b83c7111d8bb761c4baa94
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-07-24 22:04:52 -0700

    Home page complete with slideshow and menu buttons

--- vCounselor/app/screens/main_screen/main_screen.tsx
+++ vCounselor/app/screens/main_screen/main_screen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions, Image } from "react-native";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import {
   createAppContainer,
@@ -13,12 +13,24 @@ import {
   Layout,
   Text,
   Button,
+  ButtonGroup,
 } from "@ui-kitten/components";
+import {SliderBox} from "react-native-image-slider-box";
 
 let deviceHeight = Dimensions.get("window").height;
 let deviceWidth = Dimensions.get("window").width;
 
 export class MainScreen extends React.Component {
+  
+  state = {
+    images: [
+      "https://source.unsplash.com/1024x1000/?college",
+      "https://source.unsplash.com/1024x1000/?book",
+      "https://source.unsplash.com/1024x1000/?desk",
+      "https://source.unsplash.com/1024x1000/?library",
+    ]
+  };
+
   navigateToCounselingScreen = () => {
     this.props.navigation.navigate("CounselingScreen");
   }
@@ -34,30 +46,36 @@ export class MainScreen extends React.Component {
   render() {
     return (
       <Layout style={styles.container}>
+        <Image source={require('/home/arvind/Documents/Hackathons/Flarehacks-Modhacks2020/vCounselor/vCounselor/assets/vCounselorLogo.png')} style={styles.logo}></Image>
         <Text style={styles.introText}>
           Welcome to vCounselor, your personal counseling guidance app!
         </Text>
         <View style={styles.textPadding}>
         </View>
-        <Text style={styles.introText}>
-          How may we assist you today?
-        </Text>
-        <View style={styles.buttonPadding}>
+        <View style={styles.buttonPadding}/>
+        <View style={styles.slideshow}>
+          <SliderBox
+            parentWidth={deviceWidth-30}
+            sliderBoxHeight={deviceHeight/3.4}
+            autoplay={true} 
+            circleLoop={true} 
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            images={this.state.images}/>
         </View>
-        <Button onPress={() => this.navigateToCounselingScreen()}>
-          Counseling
-        </Button>
-        <View style={styles.buttonPadding}>
-        </View>
-        <Button onPress={() => this.navigateToResourcesScreen()}>
-          Resources
-        </Button>
-        <View style={styles.buttonPadding}>
-        </View>
-        <Button onPress={() => this.navigateToAboutScreen()}>
-          About
-        </Button>
-        <View style={styles.buttonPadding}>
+        <View style={styles.buttonPadding}/>
+        <View style={styles.buttonRow}>
+          <ButtonGroup>
+            <Button onPress={() => this.navigateToCounselingScreen()} style={styles.buttonStyle}>
+              Counseling
+            </Button>
+            <Button onPress={() => this.navigateToResourcesScreen()} style={styles.buttonStyle}>
+              Resources
+            </Button>
+            <Button onPress={() => this.navigateToAboutScreen()} style={styles.buttonStyle}>
+              About
+            </Button>
+          </ButtonGroup>
         </View>
       </Layout>
     );
@@ -78,11 +96,27 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   textPadding: {
-    height: 30,
+    height: 20,
     width: deviceWidth,
   },
   buttonPadding: {
     height:40,
     width: deviceWidth,
+  },
+  logo: {
+    height: deviceWidth/2,
+    width: deviceWidth/2,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    width: deviceWidth/3.25,
+  },
+  slideshow: {
+    height: deviceHeight/3,
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
