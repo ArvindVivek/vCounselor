5b572e1473dac4c138711abc40ab811ac1203902
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-07-25 23:00:15 -0700

    removed wait function

--- vCounselor/app/screens/counseling_screen/counseling_screen.tsx
+++ vCounselor/app/screens/counseling_screen/counseling_screen.tsx
@@ -35,12 +35,6 @@ async function fetchData() {
   });
 }
 
-function wait(timeout: number) {
-  return new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-}
-
 export class CounselingScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -53,7 +47,6 @@ export class CounselingScreen extends React.Component {
   }
 
   _onRefresh = () => {
-    wait(200);
     this.setState({refreshing: true});
     fetchData().then(() => {
       this.setState({refreshing: false});
